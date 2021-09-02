eaf805da8d42a020428daab723dc89572e588c9e
saschroeder <saschroeder@techfak.uni-bielefeld.de>
2021-09-02 11:25:08 +0200

    add bert mlm to __init__.py

--- embedding/__init__.py
+++ embedding/__init__.py
@@ -1,5 +1,6 @@
 from .bert_ukplab import BertUKPLab
 from .bert_huggingface import BertHuggingface
+from .bert_huggingface_mlm import BertHuggingfaceMLM
 from .use_embedder import USEEmbedder
 from .doc2vec import Doc2Vec
 
