850bd5496675001e3c8dd13d7b5f37ddfb1f7af0
Robert Feldhans <rfeldhans@techfak.uni-bielefeld.de>
2022-02-12 13:46:27 +0100

    old attention stuff

--- embedding/bert_huggingface.py
+++ embedding/bert_huggingface.py
@@ -7,29 +7,43 @@ from transformers import BertForSequenceClassification, BertTokenizer, AdamW
 import torch
 from torch.nn import functional as F
 import sklearn
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics.classification import precision_recall_fscore_support
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+import string
+
+
+def filter_stopwords(words, embed):
+    words_new = []
+    for w in zip(words, embed):
+        if w[0] not in ENGLISH_STOP_WORDS and w[0] not in string.punctuation:
+            words_new.append(w)
+    words_n, embed_n = zip(*words_new)
+    return words_n, np.array(embed_n)
 
 
 def compute_cosine_similarities(X, x):
     return sklearn.metrics.pairwise.cosine_similarity(X, np.array([x]))
 
+
 def first_zero(arr):
-    mask = arr==0
+    mask = arr == 0
     return np.where(mask.any(axis=1), mask.argmax(axis=1), -1)
+
 
 def find_maxes(X, num):
     l = list(enumerate(X))
     l[0] = (0, -10)
-    l[-1] = (len(l)-1, -10)
+    l[-1] = (len(l) - 1, -10)
     maxes = []
-    for i in range(min(num, len(l)-2)):
+    for i in range(min(num, len(l) - 2)):
         maxes.append(max(l, key=(lambda x: x[1]))[0])
         l[maxes[-1]] = (maxes[-1], -10)
     return maxes
 
+
 def normalized(a, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
-    l2[l2==0] = 1
+    l2[l2 == 0] = 1
     return a / np.expand_dims(l2, axis)
 
 
@@ -51,7 +65,10 @@ class BertHuggingface(Embedder):
     def prepare(self, **kwargs):
         model_name = kwargs.pop('model_name') or 'bert-base-uncased'
 
-        self.model = BertForSequenceClassification.from_pretrained(model_name, return_dict=True, num_labels=self.num_labels, output_hidden_states=True)
+        self.model = BertForSequenceClassification.from_pretrained(model_name, return_dict=True,
+                                                                   num_labels=self.num_labels,
+                                                                   output_hidden_states=True,
+                                                                   output_attentions=True)
         self.tokenizer = BertTokenizer.from_pretrained(model_name)
         self.__switch_to_cuda()
         self.model.eval()
@@ -70,8 +87,9 @@ class BertHuggingface(Embedder):
             out = self.model(input_ids, attention_mask=attention_mask)
             encoding = encoding.to('cpu')
 
-            hidden_states = out.hidden_states
+            attentions = out.attentions[-1].to('cpu')
 
+            hidden_states = out.hidden_states
             arr = hidden_states[-1].to('cpu')
             arr = arr.detach().numpy()
 
@@ -97,12 +115,12 @@ class BertHuggingface(Embedder):
             del input_ids
             del attention_mask
             del out
+            del attentions
             torch.cuda.empty_cache()
             if self.verbose and i % 100 == 0:
                 print("at step", i, "of", num_steps)
 
         return np.vstack(outputs)
-
 
     def save(self, path):
         self.model.save_pretrained(path)
@@ -192,17 +210,17 @@ class BertHuggingface(Embedder):
             del partial_labels
         self.model.eval()
         torch.cuda.empty_cache()
-        
+
     def __combine_words(self, input_ids, non_zeros):
         combis = []
         actual_words = []
         for n, each in enumerate(input_ids):
             word = self.tokenizer.decode([each])
             if word.startswith('##'):
-                if combis and combis[-1][-1] == n-1:
+                if combis and combis[-1][-1] == n - 1:
                     combis[-1].append(n)
                 else:
-                    combis.append([n-1,n])
+                    combis.append([n - 1, n])
                 actual_words[-1] = actual_words[-1] + word[2:]
             else:
                 actual_words.append(word)
@@ -213,9 +231,66 @@ class BertHuggingface(Embedder):
         combi_values = np.array(combi_values)
         return_non_zeros = list(non_zeros)
         for x in reversed(range(len(combis))):
-            return_non_zeros[combis[x][0]:combis[x][-1]+1] = combi_values[x]
+            return_non_zeros[combis[x][0]:combis[x][-1] + 1] = combi_values[x]
         return_non_zeros = np.array(return_non_zeros, dtype=float)
         return actual_words, return_non_zeros
+
+    def embedding_strength(self, text_list, token_per_embedding=3):
+        outputs = []
+        num_steps = int(math.ceil(len(text_list) / self.batch_size))
+        for i in range(num_steps):
+            ul = min((i + 1) * self.batch_size, len(text_list))
+            partial_input = text_list[i * self.batch_size:ul]
+            encoding = self.tokenizer(partial_input, return_tensors='pt', padding=True, truncation=True)
+            if torch.cuda.is_available():
+                encoding = encoding.to('cuda')
+            input_ids = encoding['input_ids']
+            attention_mask = encoding['attention_mask']
+            out = self.model(input_ids, attention_mask=attention_mask)
+            encoding = encoding.to('cpu')
+
+            hidden_states = out.hidden_states
+
+            arr = hidden_states[-1].to('cpu')
+            arr = arr.detach().numpy()
+
+            attention_mask = attention_mask.to('cpu')
+            att_mask = attention_mask.detach().numpy()
+
+            zeros = first_zero(att_mask)
+            array = []
+            attentions = []
+            output = []
+            for entry in range(len(partial_input)):
+                attention_masked_non_zero_entries = arr[entry]
+                if zeros[entry] > 0:
+                    attention_masked_non_zero_entries = attention_masked_non_zero_entries[:zeros[entry]]
+                actual_words, combined_entries = self.__combine_words(input_ids[entry],
+                                                                      attention_masked_non_zero_entries)
+                actual_words, combined_entries = filter_stopwords(actual_words, combined_entries)
+                array.append(
+                    np.mean(combined_entries, axis=0))  # switch combined entries and attention_masked_non_zero_entries
+                cosines = compute_cosine_similarities(combined_entries, array[-1])
+                indicies = find_maxes(cosines, token_per_embedding)
+                tokens = [actual_words[i] for i in indicies]
+                for _ in range(token_per_embedding - len(tokens)):
+                    tokens.append('_')
+                output.append(tokens)
+
+            outputs.append(output)
+            out = out.logits
+            out = out.to('cpu')
+
+            del encoding
+            del partial_input
+            del input_ids
+            del attention_mask
+            del out
+            torch.cuda.empty_cache()
+            if self.verbose and i % 100 == 0:
+                print("at step", i, "of", num_steps)
+
+        return np.vstack(outputs)
 
     def attention(self, text_list, token_per_embedding=3):
         outputs = []
@@ -247,16 +322,19 @@ class BertHuggingface(Embedder):
                 attention_masked_non_zero_entries = arr[entry]
                 if zeros[entry] > 0:
                     attention_masked_non_zero_entries = attention_masked_non_zero_entries[:zeros[entry]]
-                actual_words, combined_entries = self.__combine_words(input_ids[entry], attention_masked_non_zero_entries)
-                array.append(np.mean(combined_entries, axis=0))
+                actual_words, combined_entries = self.__combine_words(input_ids[entry],
+                                                                      attention_masked_non_zero_entries)
+                actual_words, combined_entries = filter_stopwords(actual_words, combined_entries)
+                array.append(
+                    np.mean(combined_entries, axis=0))  # switch combined entries and attention_masked_non_zero_entries
                 cosines = compute_cosine_similarities(combined_entries, array[-1])
                 indicies = find_maxes(cosines, token_per_embedding)
                 tokens = [actual_words[i] for i in indicies]
+                for _ in range(token_per_embedding - len(tokens)):
+                    tokens.append('_')
                 output.append(tokens)
 
-            embedding_output = np.asarray(output)
-
-            outputs.append(embedding_output)
+            outputs.append(output)
             out = out.logits
             out = out.to('cpu')
 
