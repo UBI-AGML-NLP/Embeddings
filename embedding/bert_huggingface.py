ea1bfcc0aa47264b9cdadbc130c02a141ba4eb0b
Sarah Schröder <saschroeder@techfak.uni-bielefeld.de>
2025-11-13 18:13:44 +0100

    fix

--- embedding/bert_huggingface.py
+++ embedding/bert_huggingface.py
@@ -147,7 +147,7 @@ class BertHuggingface(Embedder):
         outputs = np.zeros((len(texts), self.model.config.hidden_size))
         for batch in tqdm(loader, leave=True):
             if torch.cuda.is_available():
-                for key in batch.keys()
+                for key in batch.keys():
                     if key != 'index':
                         batch[key] = batch[key].to('cuda')
 
@@ -204,7 +204,7 @@ class BertHuggingface(Embedder):
         outputs = np.zeros((len(texts), self.num_labels))
         for batch in tqdm(loader, leave=True):
             if torch.cuda.is_available():
-                for key in batch.keys()
+                for key in batch.keys():
                     if key != 'index':
                         batch[key] = batch[key].to('cuda')
 
